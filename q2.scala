a2c43c78782f577756ff686f23b71bfc02638ae3
saranga_thalagalage <140165039+SARANGATHALAGALAGE@users.noreply.github.com>
2024-08-13 10:13:31 +0530

    Update q2.scala

--- q2.scala
+++ q2.scala
@@ -5,11 +5,14 @@ object NumberCategorizer {
 
     val input = scala.io.StdIn.readInt()
 
+    val isDivisibleBy3 = (num: Int) => num % 3 == 0
+    val isDivisibleBy5 = (num: Int) => num % 5 == 0
+
     def categorizeNumber(num: Int): String = {
       num match {
-        case n if n % 15 == 0 => "Multiple of Both Three and Five"
-        case n if n % 3 == 0 => "Multiple of Three"
-        case n if n % 5 == 0 => "Multiple of Five"
+        case n if isDivisibleBy3(n) && isDivisibleBy5(n) => "Multiple of Both Three and Five"
+        case n if isDivisibleBy3(n) => "Multiple of Three"
+        case n if isDivisibleBy5(n) => "Multiple of Five"
         case _ => "Not a Multiple of Three or Five"
       }
     }
